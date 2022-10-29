c84514bb8bcdefbace508c988a72983716285c4e
Hamzz <hamza.aliaskeroglu@gmail.com>
2022-10-29 17:25:06 +0300

    Updates

--- src/main/java/codespring/softwarelanguages/webApi/Controllers/ProgrammingLanguageController.java
+++ src/main/java/codespring/softwarelanguages/webApi/Controllers/ProgrammingLanguageController.java
@@ -15,34 +15,40 @@ import org.springframework.web.bind.annotation.RestController;
 
 import codespring.softwarelanguages.business.abstracts.ProgrammingLanguageService;
 import codespring.softwarelanguages.entities.concretes.ProgrammingLanguage;
-
+import io.swagger.v3.oas.annotations.*;
 
 @RestController
 @RequestMapping("/api/PLanguages")
 public class ProgrammingLanguageController {
+
+   
     private ProgrammingLanguageService pLanguageService;
 
     @Autowired
     public ProgrammingLanguageController(ProgrammingLanguageService pLanguageService) {
         this.pLanguageService = pLanguageService;
     }
-
+    @Operation(summary = "{Kayıtlı Tüm Programlama Dillerini Getirir}")
     @GetMapping("/getAll")
     public List<ProgrammingLanguage> getAll(){
         return pLanguageService.getAll();
     }
+    @Operation(summary = "{Id'e Göre Listeden Getirir}")
     @GetMapping("/getId")
     public ProgrammingLanguage getId(@RequestParam int id)throws Exception{
         return this.pLanguageService.getId(id);
     }
+    @Operation(summary = "{Listeye Yeni Kayıt Ekler}")
     @PostMapping("/setName")
     public void pLanguageAdd(@RequestBody ProgrammingLanguage pLanguage)throws Exception{
         this.pLanguageService.pLanguageAdd(pLanguage);
     }
+    @Operation(summary = "{Listede Kayıtlı İsmi Günceller}")
     @PutMapping("/updateName")
         public void pLanguageUpdate(@RequestBody ProgrammingLanguage pLanguage) {
             this.pLanguageService.pLanguageUpdate(pLanguage);
     }
+    @Operation(summary = "{Id'e Göre Listeden Kayıt Siler}")
     @DeleteMapping("/delete")
         public void pLanguageDelete(@RequestParam int id){
             this.pLanguageService.pLanguageDelete(id);
